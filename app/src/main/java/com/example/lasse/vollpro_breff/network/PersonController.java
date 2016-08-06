fc541f95ff9387ba129b646063bac7097a199bbf
lasseschneider <lars.schneider@fh-erfurt.de>
2016-08-06 17:14:00 +0200

    DS

--- app/src/main/java/com/example/lasse/vollpro_breff/network/PersonController.java
+++ app/src/main/java/com/example/lasse/vollpro_breff/network/PersonController.java
@@ -3,13 +3,11 @@ package com.example.lasse.vollpro_breff.network;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 
 /**
- * Created by Lasse on 05.07.2016.
+ * Created by Lasse on 14.07.2016.
  */
-public interface networkController {
-
+public interface PersonController {
     //retrofit und Vorlesung Netzwerkommunikation
     String BASE_URL = "localhost:8080/api/";
 
